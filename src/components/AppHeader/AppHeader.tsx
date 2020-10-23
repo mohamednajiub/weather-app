e7e12abe84d849584a53174ef78942398239f6b8
Mohamed Najiub <mohamednajiub@gmail.com>
2020-10-23 17:55:36 +0200

    ui: finish app header ui style

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-
+import * as styles from './AppHeader.module.scss';
 
 const AppHeader = () => {
     return (
-        <header>
+        <header className={styles.AppHeader}>
             <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-6">
-                        <h1>instaweather</h1>
+                <div className="row align-items-center justigy-content-between">
+                    <div className="col-8 col-md-6">
+                        <h1 className="font-bold">INSTAWEATHER</h1>
                     </div>
                     
-                    <div className="col-6 measure-units">
+                    <div className="col-4 col-md-6 measure-units d-flex justify-content-end">
                         <button type="button">C</button>
-                        <button type="button">F</button>
+                        <button type="button" className={styles.active}>F</button>
                     </div>
                 </div>
-                
             </div>
         </header>
     )
