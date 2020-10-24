83253a3cd11a270144263931d8806601e69eeb8e
Mohamed Najiub <mohamednajiub@gmail.com>
2020-10-24 14:53:39 +0200

    feat: add weather status Component

---
+++ src/components/WeatherStatusIcon/WeatherStatusIcon.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+import Sunny from '../../images/sunny.png';
+import ClearNight from '../../images/clear-night.png';
+import PartlyCloudDay from '../../images/partly-cloud-day.png';
+import PartlyCloudNight from '../../images/partly-cloudy-night.png';
+import Rainy from '../../images/rainy.png';
+import Foggy from '../../images/foggy.png';
+import Windy from '../../images/windy.png';
+import Cloudy from '../../images/cloudy.png';
+import Snow from '../../images/snow.png';
+
+interface WeatherStatusIconProps {
+    icon: string;
+    summary: string;
+}
+
+const WeatherStatusIcon: React.FC<WeatherStatusIconProps>  = ({icon, summary}) => {
+
+    const weatherIcons: any = {
+        "clear-day": Sunny,
+        "clear-night": ClearNight,
+        "partly-cloudy-day": PartlyCloudDay,
+        "partly-cloudy-night": PartlyCloudNight,
+        "rain": Rainy,   
+        "foggy": Foggy,
+        "wind": Windy,
+        "snow": Snow,
+        "cloudy": Cloudy,
+    };
+        
+    return (
+        <div>
+            <img src={weatherIcons[`${icon}`]} alt={summary} />
+        </div>
+    );
+    
+}
+
+export default WeatherStatusIcon;
