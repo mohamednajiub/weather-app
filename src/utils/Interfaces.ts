f28c829a37e4840b6be865a711fecd62ba828679
Mohamed Najiub <mohamednajiub@gmail.com>
2020-10-24 02:34:59 +0200

    utils: add app weather API response interface

--- src/utils/Interfaces.ts
+++ src/utils/Interfaces.ts
@@ -2,3 +2,42 @@ export interface GeoLocationAPIResponse {
     city: string,
     country_name: string,
 }
+
+export interface WeatherAPIResponse {
+    currently: {
+        apparentTemperature: number,
+        temperature: number,
+        icon: string,
+        time: number,
+        summary: string
+    },
+
+    daily: {
+        data: {
+            [index: number]: {
+                apparentTemperatureHigh: number,
+                apparentTemperatureLow: number,
+                apparentTemperatureMax: number,
+                apparentTemperatureMin: number,
+                temperatureHigh: number,
+                temperatureLow: number,
+                temperatureMax: number,
+                temperatureMin: number,
+                icon: string,
+                summary: string,
+            };
+        },
+        icon: string,
+        summary: string
+    },
+
+    hourly: {
+        [index: number]: {
+            time: string,
+            apparentTemperature: number,
+            temperature: number,
+            icon: string,
+            summary: string,
+        };
+    }
+}
